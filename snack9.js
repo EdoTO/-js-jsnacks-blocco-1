36dc28aeb305ec03f2dfd386c9645e75c065a3b7
EdoTO <edobusti@gmail.com>
2022-02-17 16:35:51 +0000

    update

---
+++ snack9.js
@@ -0,0 +1,16 @@
+let sum = 0
+let media = 0
+const numberQuantity = 10;
+
+
+for (let i = 1; i <= numberQuantity; i++) {
+
+    const number = i;
+
+    sum += number;
+    
+}
+
+console.log(sum)
+
+media = sum / numberQuantity;
